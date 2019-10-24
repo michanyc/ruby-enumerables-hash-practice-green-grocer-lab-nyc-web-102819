57fa1b5e1e53c08bb0c1418254ebbcdccf3dd091
Michael Gerland <mr.gerland@gmail.com>
2019-10-24 20:54:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,18 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  final_hash = {}
+  cart.each do |element_hash|
+    element_name = element_hash.key[0]
+    element-stats = element_hash.value[0]
+
+    if final_hash.has_key?(element_name)
+      element_stats[:count] = 1
+    else
+      final_hash[elements_name] = elements_stats
+      final_hash[:count] = 1
+    end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
